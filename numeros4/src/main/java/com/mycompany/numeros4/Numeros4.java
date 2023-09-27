78bd5e60086112012e4fd5218ccd9347b9fd1a78
EdgarSilos <145420257+EdgarSilos@users.noreply.github.com>
2023-09-27 08:26:13 -0600

    Add files via upload

---
+++ numeros4/src/main/java/com/mycompany/numeros4/Numeros4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.numeros4;
+
+/**
+ *
+ * @author User
+ */
+public class Numeros4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
